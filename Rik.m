8ba5b3062b7698c97cb25f1c941a1c1688d50e0a
pkgdemon <pkgdemonteam@gmail.com>
2025-04-29 14:53:11 -0500

    Remove duplicate method

--- Rik.m
+++ Rik.m
@@ -50,14 +50,6 @@ static Class _menuRegistryClass;
   return self;
 }
 
-+ (NSColor *)controlStrokeColor
-{
-  return RETAIN([NSColor colorWithCalibratedRed:0.4
-                                          green:0.4
-                                           blue:0.4
-                                          alpha:1]);
-}
-
 + (NSColor *) controlStrokeColor
 {
 
