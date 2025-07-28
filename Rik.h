1ad32ac9504930040734a2bb184ce99ef9b847d3
pkgdemon <pkgdemonteam@gmail.com>
2025-07-28 12:05:53 -0500

    Restore original code without dbus or experimental panel ipc

--- Rik.h
+++ Rik.h
@@ -9,35 +9,12 @@
 #define RIKLOG(args...) 
 #endif
 
-// Embedded protocol for panel integration - no external dependency needed
-@protocol GSMenuPanelService
-- (void)registerApplication:(NSString *)appId;
-- (void)unregisterApplication:(NSString *)appId;
-- (void)setMainMenu:(in bycopy NSMenu *)menu forApplication:(NSString *)appId;
-- (void)applicationDidBecomeActive:(NSString *)appId;
-- (void)applicationDidResignActive:(NSString *)appId;
-@end
-
 @interface Rik: GSTheme
-{
-    id menuRegistry;                          // existing DBusMenu registry
-    id<GSMenuPanelService> panelService;      // Panel service connection
-    NSConnection *panelConnection;            // Connection object to keep alive
-    NSString *currentAppId;                   // current app identifier
-    NSTimer *connectionRetryTimer;            // timer for panel connection retries
-}
-
 + (NSColor *) controlStrokeColor;
 - (void) drawPathButton: (NSBezierPath*) path
                      in: (NSCell*)cell
 			            state: (GSThemeControlState) state;
-
-// Panel integration methods
-- (void)connectToPanelService;
-- (void)disconnectFromPanelService;
-- (BOOL)shouldUseDistributedMenu:(NSMenu*)menu;
-- (void)retryPanelConnection:(NSTimer*)timer;
-
 @end
+
 
 #import "Rik+Drawings.h"
